dd046092e4470a59b2f2af968b3931c53d9157aa
Unknown <lutleyo@gmail.com>
2018-07-29 13:33:45 +0100

    Added basic bot with tokens file
    Added a basic bot that replies to "ping" messages. Logs all messages to console. Creates a token file if it doesn't exist. Loads bot token from said file.

---
+++ discord-bot/Classes/Tokens/TokenHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace discord_bot.Classes
+{
+    public static class TokenHelper
+    {
+        private static Dictionary<string, string> _tokens;
+        private static string _tokenFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tokens.xml");
+
+        public static async Task LoadTokens()
+        {
+            _tokens = new Dictionary<string, string>();
+
+            if (!File.Exists(_tokenFile))
+            {
+                CreateTokensFile();
+                return;
+            }
+
+            string text = File.ReadAllText(_tokenFile);
+            XElement rootElement = XElement.Parse(text);
+            foreach (var el in rootElement.Elements())
+            {
+                _tokens.Add(el.Name.LocalName, el.Value);
+            }
+        }
+
+        private static void CreateTokensFile()
+        {
+            _tokens = new Dictionary<string, string>();
+
+            XElement el = new XElement("root", _tokens.Select(kv => new XElement(kv.Key, kv.Value)));
+
+            el.Save(_tokenFile);
+        }
+
+        public static Dictionary<string, string> GetAllTokens()
+        {
+            return _tokens;
+        }
+
+        public static bool TokensLoaded()
+        {
+            return _tokens == null;
+        }
+
+        public static bool AtLeastOneTokenExists()
+        {
+            return _tokens.Count > 0;
+        }
+
+        public static bool TokenExists(string tokenKey)
+        {
+            return _tokens.ContainsKey(tokenKey);
+        }
+
+        public static string GetTokenValue(string tokenKey)
+        {
+            if (!TokenExists(tokenKey))
+                return null;
+
+            _tokens.TryGetValue(tokenKey, out string value);
+
+            return value;
+        }
+    }
+
+    [Serializable]
+    public struct Token
+    {
+        public string Name;
+        public string Value;
+
+        public Token(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ discord-bot/OliBotCore.cs
@@ -0,0 +1,63 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+using discord_bot.Classes;
+
+namespace discord_bot
+{
+    class OliBotCore
+    {
+        public DiscordClient OliBotClient;
+
+        private static OliBotCore _instance;
+        public static OliBotCore Instance => _instance ?? (_instance = new OliBotCore());
+
+        private static string OliBotTokenKey = "olibot";
+
+        static void Main(string[] args)
+        {
+            Instance.RunBot().ConfigureAwait(false).GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        public async Task RunBot()
+        {
+            await TokenHelper.LoadTokens();
+
+            if (!TokenHelper.AtLeastOneTokenExists())
+            {
+                Console.WriteLine("No tokens exist!");
+                return;
+            }
+            else if (TokenHelper.TokenExists(OliBotTokenKey))
+            {
+                Console.WriteLine($"There isn't a token for OliBot!{Environment.NewLine}Please create a token with the key: {OliBotTokenKey}");
+                return;
+            }
+
+            OliBotClient = new DiscordClient(new DiscordConfiguration
+            {
+#if DEBUG
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug,
+#endif
+                Token = TokenHelper.GetTokenValue(OliBotTokenKey),
+                TokenType = TokenType.Bot
+            });
+
+            OliBotClient.MessageCreated += async e =>
+            {
+                if (e.Author.IsBot)
+                    return;
+
+                Console.WriteLine($"New message! {e.Author.Username} said: {e.Message.Content}");
+
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await OliBotClient.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
