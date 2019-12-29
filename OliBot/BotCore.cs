7a671c1b716c77687833e80ee30bd9248bc6850c
Oliver <8377772+oliver4888@users.noreply.github.com>
2019-12-29 03:07:13 +0000

    Add ping bot.

---
+++ OliBot/BotCore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace OliBot
+{
+    
+    public class BotCore
+    {
+        static DiscordClient discord;
+        public async Task Start()
+        {
+            string token = await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory, "token.txt"));
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Author.IsBot) return;
+
+                Console.WriteLine(e.Author.Username);
+
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            discord.Ready += async e =>
+            {
+                Console.WriteLine("Ready!");
+            };
+
+            discord.SocketErrored += async e => {
+                Console.WriteLine(e.Exception);
+            };
+
+            await discord.ConnectAsync(new DiscordActivity("v2 dev"));
+
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ OliBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OliBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new BotCore().Start().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
