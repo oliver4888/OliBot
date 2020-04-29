63681a0acd2d2d2e0a153f87ee27af667d48ad12
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-04-29 22:45:54 +0100

    Target netstandard2.1. Add an empty bot that reads it's token an initial status from an appsettings.json configuration file.

--- BotCoreModule/BotCoreModule.cs
+++ BotCoreModule/BotCoreModule.cs
@@ -1,9 +1,33 @@
 ﻿using Common;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace BotCoreModule
 {
     [Module]
     public class BotCoreModule
     {
+        IConfigurationSection _config;
+
+        public static DiscordClient discordClient;
+
+        public BotCoreModule(IConfigurationRoot configuration) => _config = configuration.GetSection("BotCore");
+
+        public async Task Start()
+        {
+            discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = _config["Token"],
+                TokenType = TokenType.Bot
+            });
+
+            string status = _config["InitialStatus"];
+
+            await discordClient.ConnectAsync(status != null ? new DiscordActivity(status) : null);
+
+            await Task.Delay(-1);
+        }
     }
 }
--- BotRunner/Program.cs
+++ BotRunner/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BotRunner
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            IServiceCollection services = new ServiceCollection();
+            services.AddConfiguration();
+
             ModuleHelper.LoadModules();
-            ModuleHelper.GetModules().ToList().ForEach(module => Console.WriteLine(module.Name));
-            Console.ReadKey();
+            IEnumerable<Type> modules = ModuleHelper.GetModules();
+            Type botCore = modules.Where(module => module.Name == "BotCoreModule").FirstOrDefault();
+
+            foreach (Type module in modules)
+                services.AddSingleton(module);
+
+            (botCore.GetMethod("Start").Invoke(services.BuildServiceProvider().GetRequiredService(botCore), null) as Task).ConfigureAwait(false).GetAwaiter().GetResult();
         }
+
+        static IServiceCollection AddConfiguration(this IServiceCollection services) =>
+            services.AddSingleton(new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build());
     }
 }
