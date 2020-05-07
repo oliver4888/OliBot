3957225ddfc6e02d7a42309759fe0bce425d6a05
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-05-07 01:47:21 +0100

    Add module to delete empty messages left behind when someone deletes an embed. Remove some fields from the steam embed to reduce it's height.

--- SteamHelperModule/SteamHelperModule.cs
+++ SteamHelperModule/SteamHelperModule.cs
@@ -84,7 +84,7 @@ namespace SteamHelperModule
 
         private DiscordEmbed BuildEmbedForItem(DiscordEmbedBuilder builder, PublishedFileDetailsModel model, PlayerSummaryModel userModel)
         {
-            string description = Regex.Replace(model.Description, @"\[[^]]+\]", "");
+            string description = string.Join(" ", Regex.Replace(model.Description, @"\[[^]]+\]", "").Split(Environment.NewLine));
 
             if (model.PreviewUrl != null)
                 builder.WithImageUrl(model.PreviewUrl);
@@ -94,11 +94,11 @@ namespace SteamHelperModule
                 .WithUrl(SteamWebLinkAffix + model.PublishedFileId.ToString())
                 .WithDescription(description.Length > 200 ? description.Substring(0, 200).Trim() + "..." : description)
                 .AddField("Author", userModel.Nickname, true)
-                .AddField("Created", model.TimeCreated.ToString(), true)
+                //.AddField("Created", model.TimeCreated.ToString(), true)
                 .AddField("Last Updated", model.TimeUpdated.ToString(), true)
                 .AddField("Views", string.Format("{0:n0}", model.Views), true)
-                .AddField("Favorited", string.Format("{0:n0}", model.Favorited), true)
-                .AddField("Subscriptions", string.Format("{0:n0}", model.Subscriptions), true)
+                //.AddField("Favorited", string.Format("{0:n0}", model.Favorited), true)
+                //.AddField("Subscriptions", string.Format("{0:n0}", model.Subscriptions), true)
                 .AddField("Tags", string.Join(", ", model.Tags))
                 .AddField("Steam Client Link", SteamClientLinkAffix + model.PublishedFileId.ToString())
                 .Build();
