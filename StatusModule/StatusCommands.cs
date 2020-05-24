ca658543cc04b15e00dc23ca3ea285d645d1e4b2
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-05-24 18:44:46 +0100

    Add module to manage the bots status.

---
+++ StatusModule/StatusCommands.cs
@@ -0,0 +1,57 @@
+﻿using Common;
+using Common.Attributes;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace StatusModule
+{
+    public class StatusCommands
+    {
+        [Command(permissionLevel: BotPermissionLevel.HostOwner, groupName: "Bot Status")]
+        [Description("Changes the bots status.")]
+        public async Task SetStatus(CommandContext ctx, ActivityType type, string activity)
+        {
+            if (!StatusModule.IsValidStatus(type, activity))
+            {
+                await ctx.Channel.SendMessageAsync($"{ctx.Author.Mention}, Invalid status!");
+                return;
+            }
+
+            // Temp until command system supports params or a [RemainingText] attribute
+            string content = ctx.Message.Content;
+
+            StatusModule.StatusConfig.Mode = StatusMode.Manual;
+            StatusModule.StatusTimer.Stop();
+            await StatusModule.SetStatus(type, content[content.IndexOf(activity)..]);
+        }
+
+        [Command(permissionLevel: BotPermissionLevel.HostOwner, groupName: "Bot Status")]
+        [Description("Adds the new custom status.")]
+        public async Task AddStatus(CommandContext ctx, ActivityType type, string activity)
+        {
+            if (!StatusModule.IsValidStatus(type, activity))
+            {
+                await ctx.Channel.SendMessageAsync($"{ctx.Author.Mention}, Invalid status!");
+                return;
+            }
+
+            // Temp until command system supports params or a [RemainingText] attribute
+            string content = ctx.Message.Content;
+
+            await StatusModule.AddStatus(type, content[content.IndexOf(activity)..]);
+        }
+
+        [Command(permissionLevel: BotPermissionLevel.HostOwner, groupName: "Bot Status")]
+        [Description("Changes the StatusMode.")]
+        public void SetStatusMode(CommandContext ctx, StatusMode mode)
+        {
+            StatusModule.StatusConfig.Mode = mode;
+
+            if (StatusModule.StatusConfig.Mode == StatusMode.Automatic) {
+                StatusModule.StatusTimer.Start();
+            } else {
+                StatusModule.StatusTimer.Stop();
+            }
+        }
+    }
+}
---
+++ StatusModule/StatusModule.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using System.Text.Json;
+using Common.Attributes;
+using Common.Interfaces;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace StatusModule
+{
+    [Module]
+    public class StatusModule
+    {
+        internal static ILogger<StatusModule> _logger;
+        internal static IBotCoreModule _botCoreModule;
+
+        const string _statusConfigFileName = "StatusModuleConfig.json";
+        static string _configFile;
+
+        internal readonly static Timer StatusTimer = new Timer(30 * (60 * 1000)); // Every 30 minutes
+        static readonly Random _random = new Random();
+
+        internal static StatusModuleConfig StatusConfig { get; set; }
+
+        public StatusModule(ILogger<StatusModule> logger, IBotCoreModule botCoreModule)
+        {
+            _logger = logger;
+            _botCoreModule = botCoreModule;
+
+            _botCoreModule.CommandHandler.RegisterCommands<StatusCommands>();
+            _botCoreModule.DiscordClient.Ready += async (ReadyEventArgs e) => await SetRandomStatus();
+
+            _configFile = Path.Combine(Environment.CurrentDirectory, _statusConfigFileName);
+
+            if (!File.Exists(_configFile))
+            {
+                _logger.LogInformation($"{nameof(StatusModule)}: No config file found. Creating default config at {_configFile}");
+                StatusConfig = new StatusModuleConfig
+                {
+                    Mode = StatusMode.Automatic
+                };
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                AddStatus(ActivityType.ListeningTo, "Rick Astley", false);
+                AddStatus(ActivityType.Playing, "Minecraft");
+            }
+            else
+                LoadConfig();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            StatusTimer.Elapsed += async (object sender, ElapsedEventArgs e) =>
+            {
+                await SetRandomStatus();
+            };
+            StatusTimer.Start();
+        }
+
+        internal static bool IsValidStatus(ActivityType type, string message) =>
+            !(type == ActivityType.Streaming || type == ActivityType.Custom || string.IsNullOrWhiteSpace(message));
+
+        internal static async Task SetStatus(ActivityType type, string message) =>
+            await _botCoreModule.DiscordClient.UpdateStatusAsync(new DiscordActivity(message, type));
+
+        internal static async Task<bool> AddStatus(ActivityType type, string message, bool save = true)
+        {
+            if (!IsValidStatus(type, message))
+                return false;
+
+            StatusConfig.Statuses.Add(new Status { ActivityType = type, Message = message });
+
+            if (save)
+                await SaveConfig();
+            return true;
+        }
+
+        internal static async Task SetRandomStatus()
+        {
+            Status status = StatusConfig.Statuses[_random.Next(0, StatusConfig.Statuses.Count)];
+            await SetStatus(status.ActivityType, status.Message);
+        }
+
+        internal static async Task SaveConfig()
+        {
+            try
+            {
+                using FileStream fs = File.Create(_configFile);
+                await JsonSerializer.SerializeAsync(fs, StatusConfig, new JsonSerializerOptions { WriteIndented = true });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error saving {nameof(StatusModule)} config");
+            }
+        }
+
+        internal static async Task LoadConfig()
+        {
+            try
+            {
+                using FileStream fs = File.OpenRead(_configFile);
+                StatusConfig = await JsonSerializer.DeserializeAsync<StatusModuleConfig>(fs);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error loading {nameof(StatusModule)} config");
+            }
+        }
+    }
+}
