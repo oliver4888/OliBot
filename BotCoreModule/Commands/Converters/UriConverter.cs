c65c582a04a0eb03a1ce610e242df6e52250162e
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-08-15 23:36:14 +0100

    Add Uri converter for URLs.

---
+++ BotCoreModule/Commands/Converters/UriConverter.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using System;
+using Common.Interfaces;
+
+namespace BotCoreModule.Commands.Converters
+{
+    public class UriConverter : IConverter<Uri>
+    {
+        public bool TryParse(string input, CommandContext ctx, out Uri parsedValue)
+        {
+            parsedValue = null;
+
+            if (!Uri.IsWellFormedUriString(input, UriKind.Absolute))
+                return false;
+
+            Uri uri = new Uri(input);
+
+            if (uri.IsFile)
+                return false;
+
+            parsedValue = uri;
+            return true;
+        }
+    }
+}
