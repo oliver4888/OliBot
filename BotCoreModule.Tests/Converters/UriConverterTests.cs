06e29849e6dd23a6ba404d9540ad955a9def4e7b
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-08-17 21:42:08 +0100

    Add some tests for the Uri Converter.

---
+++ BotCoreModule.Tests/Converters/UriConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using BotCoreModule.Commands.Converters;
+
+namespace BotCoreModule.Tests.Converters
+{
+    public class UriConverterTests
+    {
+        readonly UriConverter _converter = new UriConverter();
+
+        [TestCase("", false, "")]
+        [TestCase("https://oliver4888.com", true, "https://oliver4888.com/")]
+        public void WillParseCorrectly(string input, bool willConvert, string expectedOutput)
+        {
+            Assert.AreEqual(willConvert, _converter.TryParse(input, null, out Uri result));
+
+            if (willConvert)
+                // I'm not sure of a better way to compare since attribute arguments must be constant
+                Assert.AreEqual(expectedOutput, result.AbsoluteUri);
+        }
+
+        [TestCase("file://C:/path/file.xml")]
+        [TestCase("\\\\serverName\\path\\to\\file.css")]
+        [TestCase("C:/path/to/file.txt")]
+        public void WillRejectLocalPaths(string input)
+        {
+            Assert.IsFalse(_converter.TryParse(input, null, out _));
+        }
+    }
+}
